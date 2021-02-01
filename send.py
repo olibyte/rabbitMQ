55c961c5b7af6d0c6b6e0e83a9281ce2b79ccef6
Oliver <ocben1@gmail.com>
2021-02-02 08:17:58 +1100

    initial commit

---
+++ send.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',routing_key='hello',body='Hello World!')
+print(" [x] sent 'Hello World!'")
+
+connection.close()
